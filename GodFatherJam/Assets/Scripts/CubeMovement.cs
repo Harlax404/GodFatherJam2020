16a27e4205fed907b9b5069f9f9e41fcb9a2b4bd
Julien Dor <julien.dor3@gmail.com>
2020-09-02 17:15:41 +0200

    First version of the cube manager

--- GodFatherJam/Assets/Scripts/CubeMovement.cs
+++ GodFatherJam/Assets/Scripts/CubeMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class CubeMovement : MonoBehaviour
 {
-    public Vector3 offset;
+        
+    
 
     public GameObject entity;
 
@@ -18,59 +19,52 @@ public class CubeMovement : MonoBehaviour
     public int step = 9;
 
     public float speed = 0.01f;
+    public float speed2 = 1.0f;
 
-    private bool input = true;
-
-    public List<GameObject> controlPoints = new List<GameObject>();
-    
-    // 4 raycasts des côtés
+    [SerializeField]
+    private List<GameObject> controlPoints = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine("Pathing");
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if(input)
+        /*if (!Physics.Linecast(transform.position, controlPoints[0].transform.position))
         {
-            if(Input.GetKey(KeyCode.Z) && input)
+            //StartCoroutine("moveRight");
+            transform.position = Vector3.MoveTowards(transform.position, controlPoints[0].transform.position, Time.deltaTime * speed2);
+            Debug.Log("Go 0");
+            if(transform.position == controlPoints[0].transform.position)
             {
-                StartCoroutine("moveUp");
-                input = false;
-            }
 
-            if (Input.GetKey(KeyCode.S) && input)
-            {
-                StartCoroutine("moveDown");
-                input = false;
+                Debug.Log("Stop");
             }
+        }*/
 
-            if (Input.GetKey(KeyCode.D) && input)
-            {
-                StartCoroutine("moveRight");
-                input = false;
-            }
-
-            if (Input.GetKey(KeyCode.Q) && input)
-            {
-                StartCoroutine("moveLeft");
-                input = false;
-            }
-        }
+        
+    
     }
 
     IEnumerator moveUp()
     {
+        //Rotating
         for (int i = 0; i < (90 / step); i++)
         {
             entity.transform.RotateAround(up.transform.position, Vector3.right, step);
             yield return new WaitForSeconds(speed);
         }
+        
+
         center.transform.position = entity.transform.position;
-        input = true;
+
+
+        
+
     }
 
     IEnumerator moveDown()
@@ -81,7 +75,8 @@ public class CubeMovement : MonoBehaviour
             yield return new WaitForSeconds(speed);
         }
         center.transform.position = entity.transform.position;
-        input = true;
+
+        
     }
 
     IEnumerator moveLeft()
@@ -92,7 +87,8 @@ public class CubeMovement : MonoBehaviour
             yield return new WaitForSeconds(speed);
         }
         center.transform.position = entity.transform.position;
-        input = true;
+
+        
     }
 
     IEnumerator moveRight()
@@ -103,6 +99,63 @@ public class CubeMovement : MonoBehaviour
             yield return new WaitForSeconds(speed);
         }
         center.transform.position = entity.transform.position;
-        input = true;
+
+        
+    }
+
+    IEnumerator Pathing()
+    {
+        // for each control point in the list
+        for(int i2 = 0; i2 <= (controlPoints.Count - 1); i2++)
+        {
+            Debug.Log(i2);
+
+
+            Vector3 offset = controlPoints[i2].transform.position - transform.position;
+
+            //new Vector3(controlPoints[i2].transform.position.x - transform.position.x, controlPoints[i2].transform.position.y - transform.position.y, controlPoints[i2].transform.position.z - transform.position.z);
+
+            
+
+            // While the cube isn't at the control point's position
+            //while (controlPoints[i2].transform.position != transform.position || ((offset.x < 0.01 && offset.x > -0.01) && (offset.y < 0.01 && offset.y > -0.01) && (offset.z < 0.01 && offset.z > -0.01)))
+            while (offset.sqrMagnitude >= 0.1)
+            {
+                Debug.Log("goes in");
+                if (controlPoints[i2].tag == "Up")
+                {
+                    Debug.Log("goes up");
+                    StartCoroutine("moveUp");
+                }
+
+                if (controlPoints[i2].tag == "Down")
+                {
+                    Debug.Log("goes down");
+                    StartCoroutine("moveDown");
+                   
+                }
+
+                if (controlPoints[i2].tag == "Left")
+                {
+                    Debug.Log("goes left");
+                    StartCoroutine("moveLeft");
+                }
+
+                if (controlPoints[i2].tag == "Right")
+                {
+                    Debug.Log("goes right");
+                    StartCoroutine("moveRight");
+                }
+
+                offset = controlPoints[i2].transform.position - transform.position;
+                Debug.Log(offset.sqrMagnitude);
+                yield return new WaitForSeconds(0.5f);
+            }
+
+
+            Debug.Log("goes out");
+        }
+        StartCoroutine("Pathing");
+       
     }
 }
