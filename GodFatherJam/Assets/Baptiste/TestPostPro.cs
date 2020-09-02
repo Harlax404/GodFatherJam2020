93c832aa6f3e762974e29252f261c6a7db8f4883
Barreyre <barreyre26@gmail.com>
2020-09-02 14:29:01 +0200

    PostPro Script is GOOD

--- GodFatherJam/Assets/Baptiste/TestPostPro.cs
+++ GodFatherJam/Assets/Baptiste/TestPostPro.cs
@@ -5,32 +5,41 @@ using UnityEngine.Rendering.PostProcessing;
 
 public class TestPostPro : MonoBehaviour
 {
-    [SerializeField]
     private float PPIntesity;
+    [SerializeField]
+    private float VignetteMod = 1;
+    [SerializeField]
+    private float FieldMod = 250;
+    [SerializeField]
+    private float BloomMod = 25;
+    [SerializeField]
+    private float MaxIntensity = 1;
 
     private Vignette PPVignette;
+    private DepthOfField PPField;
+    private Bloom PPBloom;
 
     void Start()
     {
         PostProcessVolume volume = gameObject.GetComponent<PostProcessVolume>();
-        //volume.profile.TryGetSettings<Vignette>(out );
         PPVignette = volume.profile.GetSetting<Vignette>();
+        PPField = volume.profile.GetSetting<DepthOfField>();
+        PPBloom = volume.profile.GetSetting<Bloom>();
     }
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if(Input.GetKey(KeyCode.Space) && PPIntesity < MaxIntensity)
         {
             PPIntesity += 0.01f;
+            UpdatePostProcess(PPIntesity);
         }
-
-        UpdatePostProcess(PPIntesity);
     }
 
     private void UpdatePostProcess(float ModifIntensity)
     {
-        PPVignette.intensity.value = ModifIntensity;
-
-        // Set tous les bails en fonction de l'intensité
+        PPVignette.intensity.value = ModifIntensity * VignetteMod;
+        PPField.focalLength.value = ModifIntensity * FieldMod;
+        PPBloom.intensity.value = ModifIntensity * BloomMod;
     }
 }
