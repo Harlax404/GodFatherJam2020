5415580f521ff071f70c7ed5cd6a709e41843100
Harlax <alexis.sainsard@gmail.com>
2020-09-03 14:27:04 +0200

    gamemanager

---
+++ GodFatherJam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,92 @@
+﻿using JetBrains.Annotations;
+using Rewired;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    private static GameManager _instance = null;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    #endregion
+
+    #region Variables
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    public bool alarmMode = false;
+    #endregion
+
+    public void PlayPause()
+    {
+        // to do : mettre le jeu en pause et le relancer
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void QuitGame()
+    {
+        //to do : quitter le jeu
+        Application.Quit();
+        Debug.Log("quit");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                PlayPause();
+            }
+        }
+    }
+}
