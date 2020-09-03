c057600b2de1d66a3bea07839e0e97b2cfaada24
Barreyre <barreyre26@gmail.com>
2020-09-03 23:15:18 +0200

    Menu + Credits Done (Need title)

---
+++ GodFatherJam/Assets/Scripts/Boutons.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Boutons : MonoBehaviour
+{
+    public string SceneToLoad;
+
+    public void LoadDaScene(string SceneToLoad)
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
