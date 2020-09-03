f62945e0c37bcd60ebe80d56bf9cbc799b0a6b78
Barreyre <barreyre26@gmail.com>
2020-09-03 19:14:38 +0200

    Added CameraScript

---
+++ GodFatherJam/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Vector3 offset = new Vector3(0, 4, -5);
+
+    [SerializeField]
+    private float dampingSpeed = 5.0f;
+
+    private void Start()
+    {
+        Vector3 targetPosition = target.position + offset;
+        transform.position = targetPosition;
+    }
+
+    private void Update()
+    {
+        Vector3 targetPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, dampingSpeed * Time.deltaTime);
+
+        Debug.DrawLine(transform.position ,targetPosition, Color.red);
+    }
+}
